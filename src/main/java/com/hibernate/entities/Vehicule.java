5aa263abe7d9ed12c1b08172532f8a16e7b9a354
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-08 11:05:54 +0100

    Update Vehicule.java

--- src/main/java/com/hibernate/entities/Vehicule.java
+++ src/main/java/com/hibernate/entities/Vehicule.java
@@ -7,14 +7,26 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "Type")
+@Inheritance(strategy = InheritanceType.JOINED) // pour chaque héritage on aura une relation par id
+@DiscriminatorColumn(name = "Type") // sera un enum de sous table
 @TableGenerator(name = "Vehicule_GEN", table = "Id_GEN",
 pkColumnName = "GEN_name", valueColumnName = "GEN_value", allocationSize= 1)
 public class Vehicule {
 @Id
 long idVehicule;
 String manufacturer;
+public long getIdVehicule() {
+	return idVehicule;
+}
+public void setIdVehicule(long idVehicule) {
+	this.idVehicule = idVehicule;
+}
+public String getManufacturer() {
+	return manufacturer;
+}
+public void setManufacturer(String manufacturer) {
+	this.manufacturer = manufacturer;
+}
 
 
 
