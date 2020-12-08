776acc6642b2b703b662a5a5f8a84015f75345cb
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-08 11:07:40 +0100

    Create Passenger.java

---
+++ src/main/java/com/hibernate/entities/Passenger.java
@@ -0,0 +1,16 @@
+package com.hibernate.entities;
+
+import javax.persistence.MappedSuperclass;
+@MappedSuperclass
+public class Passenger extends Vehicule {
+int noOfpassengers;
+public int getNoOfpassengers() {
+return noOfpassengers;
+}
+public void setNoOfpassengers(int noOfpassengers) {
+this.noOfpassengers = noOfpassengers;
+}
+
+
+
+}
